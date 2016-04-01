036bca70f822b468d1bc63ee9db86fb1e2ad74c2
taylor-oconnell <t89oconnell@gmail.com>
2016-04-01 14:53:00 -0700

    Adding code to write file with role:fid:sequence.

--- get_protein_encoding_genes.py
+++ get_protein_encoding_genes.py
@@ -96,39 +96,19 @@ def match_roles_and_seqs(fids_seqs, fids_roles):
 if __name__ == '__main__':
 
     genome_id = '83333.1'
-
+    """
     # Get all of the fids for the pegs in the genome
     fids = get_pegs(genome_id)
-    #fids = fids[0:25]
     print "# of pegs in genome: " + str(len(fids)) + "\n"
-    #fout_pegs = open('pegs_in_genome.txt', 'w')
-    #for f in fids:
-        #fout_pegs.write(f + "\n")
-    #fout_pegs.close()
-
-    # Read in the roles present in the model
-    roles = []
-    fin = open('roles_present.txt', 'r')
-    for line in fin:
-        roles.append(line.strip())
-
-    #Get the fids in the genome associated with the roles
-    roles_fids = get_fids_for_roles(roles, genome_id)
-    print("# of role/fid pairs: " + str(len(roles_fids)))
-
-    count0 = 0
-    for r in roles_fids:
-        if len(roles_fids[r]) > 1:
-            print(r + "\t" + str(roles_fids[r]))
-            count0 = count0 + 1
-    print("# of roles with multiple fids: " + str(count0))
-
-
-    
+    fout_pegs = open('pegs_in_genome.txt', 'w')
+    for f in fids:
+        fout_pegs.write(f + "\n")
+    fout_pegs.close()
+    """
+    """
     # Get the protein sequences for each feature (gene) in the genome.
     prot_seqs = get_prot_seqs(fids)
     print "\n# of fids: " + str(len(prot_seqs.keys()))
-    print "\n# of protein sequences: " + str(len(prot_seqs.values()))
     
     #fout_ps = open('pegs_and_seqs.txt', 'w')
     #for p in prot_seqs:
@@ -140,14 +120,54 @@ if __name__ == '__main__':
         if i == []:
             count1 = count1 + 1
     print("# of fids with no prot sequence: " + str(count1))
-
-        
     """
-    # Match the roles and sequences together using fids
-    roles_and_seqs = match_roles_and_seqs(prot_seqs, roles)
+    # Read in all the fid:sequence pairs in the genome
+    fid_seq = {}
+    fin1 = open('pegs_and_seqs.txt', 'r')
+    for line in fin1:
+        [fid, seq] = line.strip().split()
+        fid_seq[fid] = seq
+    fin1.close()
+    
+    # Read in the roles present in the model
+    roles = []
+    fin2 = open('roles_present.txt', 'r')
+    for line in fin2:
+        roles.append(line.strip())
+    fin2.close()
 
-    print roles_and_seqs
-    """
+    #Get the fids in the genome associated with the roles
+    roles_fids = get_fids_for_roles(roles, genome_id)
+    print("# of role/fid pairs: " + str(len(roles_fids)))
+
+    count0 = 0
+    count1 = 0
+    count2 = 0
+    for r in roles_fids:
+        if len(roles_fids[r]) > 1:
+            #print(r + "\t" + str(roles_fids[r]))
+            count0 = count0 + 1
+        if len(roles_fids[r]) == 1:
+            count1 = count1 + 1
+        if len(roles_fids[r]) == 0:
+            count2 = count2 + 1
+    print("# of roles with multiple fids: " + str(count0))
+    print("# of roles with one fid: " + str(count1))
+    print("# of roles with no fid: " + str(count2))
+    
+    # Write out file with role, fid, and sequence
+    fout_rfs = open('role_peg_seq.txt', 'w')
+    for r in roles_fids:
+        for f in roles_fids[r]:
+            fout_rfs.write(r + "\t" + f + "\t" + fid_seq[f] + "\n")
+    fout_rfs.close()
+    
+    
+    
+    
+
+
+
 
     
 
