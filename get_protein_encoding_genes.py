bff575618de5fd235f991dd99ff0b21ea375b121
taylor-oconnell <t89oconnell@gmail.com>
2016-04-05 15:55:25 -0700

    Adding code to write out FASTA files.

--- get_protein_encoding_genes.py
+++ get_protein_encoding_genes.py
@@ -49,8 +49,8 @@ def get_fids_for_roles(roles_present, genomeID):
     as the values.
     """
 
-    fids_roles = server.occ_of_role({'-roles':roles_present, '-genomes': [genomeID]})
-    return fids_roles
+    roles_fids = server.occ_of_role({'-roles':roles_present, '-genomes': [genomeID]})
+    return roles_fids
 
 ##############################################################################
 
@@ -120,12 +120,12 @@ if __name__ == '__main__':
         if i == []:
             count1 = count1 + 1
     print("# of fids with no prot sequence: " + str(count1))
-    """
+    
     # Read in all the fid:sequence pairs in the genome
     fid_seq = {}
     fin1 = open('pegs_and_seqs.txt', 'r')
     for line in fin1:
-        [fid, seq] = line.strip().split()
+        [fid, seq] = line.strip().split("\t")
         fid_seq[fid] = seq
     fin1.close()
     
@@ -143,7 +143,9 @@ if __name__ == '__main__':
     count0 = 0
     count1 = 0
     count2 = 0
+    num = 0
     for r in roles_fids:
+        num = num + len(roles_fids[r])
         if len(roles_fids[r]) > 1:
             #print(r + "\t" + str(roles_fids[r]))
             count0 = count0 + 1
@@ -154,13 +156,55 @@ if __name__ == '__main__':
     print("# of roles with multiple fids: " + str(count0))
     print("# of roles with one fid: " + str(count1))
     print("# of roles with no fid: " + str(count2))
-    
+    print("# of fids linked to roles: " + str(num))
+
     # Write out file with role, fid, and sequence
     fout_rfs = open('role_peg_seq.txt', 'w')
     for r in roles_fids:
         for f in roles_fids[r]:
             fout_rfs.write(r + "\t" + f + "\t" + fid_seq[f] + "\n")
     fout_rfs.close()
+    """
+    
+    
+   # Create dictionary with fids as keys and protein seqs as values
+    fin3 = open('role_peg_seq.txt', 'r')
+    peg_seq = {}
+    for line in fin3:
+        fields = line.strip().split("\t")
+        if fields[1] not in peg_seq:
+            peg_seq[fields[1]] = fields[2]
+    fin3.close()
+    
+    # Get a list of the fids that are linked to roles
+    fids_w_roles = peg_seq.keys()
+       
+    # Write out a FASTA file of the protein sequences for the fids
+    # linked to functional roles
+    fout_fasta = open('pegs_with_roles.fasta', 'w')
+    for p in peg_seq:
+        fout_fasta.write(">" + p + "\n" + peg_seq[p] + "\n")
+    fout_fasta.close()
+    
+    
+    # Write out a FASTA file of the protein sequences for all the fids
+    # present in the genome. Also write out a FASTA file of the protein
+    # sequences for all the fids that are not linked to a functional role.
+    fout_fasta2 = open('all_pegs.fasta', 'w')
+    fout_fasta3 = open('pegs_without_roles.fasta', 'w')
+    fin4 = open('pegs_and_seqs.txt', 'r')
+    for line in fin4:
+        fields = line.strip().split("\t")
+        fout_fasta2.write(">" + fields[0] + "\n" + fields[1] + "\n")
+        if fields[0] not in fids_w_roles:
+            fout_fasta3.write(">" + fields[0] + "\n" + fields[1] + "\n")
+    fout_fasta2.close()
+    fout_fasta3.close()
+    fin4.close()
+    
+
+
+
     
     
     
