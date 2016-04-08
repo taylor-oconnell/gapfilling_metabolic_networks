a9cd9768e89c4d871864e7e90fe2ce1cb4e78be8
taylor-oconnell <t89oconnell@gmail.com>
2016-04-07 17:02:21 -0700

    Adding code to compute all the rest of the physicochemical features on the fids.

--- physicochemical_features.py
+++ physicochemical_features.py
@@ -23,18 +23,41 @@ for line in fin1:
         fids_seqs[fields[1]] = fields[2]
         
 fin1.close()
-
+"""
 # Write out the roles present to a file
 fout1 = open('roles_present.txt','w')
 for r in roles:
     fout1.write(r + "\n")
 fout1.close()
-
+"""
 # Calculate the isoelectric points
 pI = calculate_pI(fids_seqs)
 firstFive = pI.keys()[:5]
 for i in firstFive:
     print(i + "\t" + str(pI[i]))
+print("\n")
+
+# Calculate the molecular weights
+MW = calculate_MW(fids_seqs)
+firstFive = MW.keys()[:5]
+for i in firstFive:
+    print(i + "\t" + str(MW[i]))
+print("\n")
+
+# Calculate the secondary structure fraction
+ssf = calculate_secondary_structure_fraction(fids_seqs)
+firstFive = ssf.keys()[:5]
+for i in firstFive:
+    print(i + "\t" + str(ssf[i]))
+print("\n")
+
+# Calculate the amino acid composition
+aac = calculate_aa_composition(fids_seqs)
+firstFive = aac.keys()[:5]
+for i in firstFive:
+    print(i + "\t" + str(aac[i]))
+print("\n")
+
 
     
 
